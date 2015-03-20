0f1117c84944df05673f193d123f2df4cc7a3e06
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2015-03-20 20:50:56 -0300

    Updated the workers code
    Some implementations

---
+++ Workers-HW/Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workers_HW
+{
+    public class Worker
+    {
+        public string nome;
+        public int idade;
+        public string profissao;
+        public float salario;
+        public int filhos;
+        public string sexo;
+        public string estadoCivil;
+        public string tipoSanguineo;
+    }
+}
